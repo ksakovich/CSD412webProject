9ecba57fa532eae7a66c1de962f48821613e9f8c
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2020-11-12 16:35:27 -0800

    Created MovieList model

---
+++ CSD412webProject/CSD412webProject/Models/MovieList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD412webProject.Models
+{
+    public class MovieList
+    {
+        private static int IdCounter = 1;
+        public int MovieListId { get; }
+        public int UserId { get; }
+        public string MovieListName { get; set; }
+        public string MovieListLink { get; set; }
+        public bool IsPublic { get; set; }
+
+        public MovieList(int userId, string name, bool isPublic)
+        {
+            MovieListId = IdCounter++;
+            MovieListName = name;
+            IsPublic = isPublic;
+            MovieListLink = null;
+        }
+    }
+}
