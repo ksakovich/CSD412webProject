7d58f744dbf207c3be8ceab70c35a915c5e73428
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2020-11-13 12:10:40 -0800

    Updated user.cs
    added few filds and methods to them

--- CSD412webProject/CSD412webProject/Models/User.cs
+++ CSD412webProject/CSD412webProject/Models/User.cs
@@ -23,16 +23,20 @@ namespace CSD412webProject.Models
         
         public string Name { get; set; }
         public string LastName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Roles UserRole { get; set; }
+        public List<int> MovieListsIds { get; }
+        public List<int> FriendsIds { get; }
 
 
-        public User(string name, string lastname, string email, bool admin)
+        public User(string name, string lastname, string username,string email, bool admin)
         {
             this.Id = IdCounter++;
             this.Name = name;
             this.LastName = lastname;
+            Username = username;
             this.Email = email;
             if (admin) 
             {
@@ -41,6 +45,62 @@ namespace CSD412webProject.Models
             else
             {
                 UserRole = Roles.Customer;
+            }
+        }
+
+        public void AddFriendId(int friendId)
+        {
+            if (FriendsIds.Contains(friendId))
+            {
+                throw new Exception("This friend ID is already in the list");
+            }
+            if (friendId < 0)
+            {
+                throw new Exception("Friend's ID cannot be negative");
+            }
+            else
+            {
+                FriendsIds.Add(friendId);
+            }
+        }
+
+        public void RemoveFriendId(int friendId)
+        {
+            if (!FriendsIds.Contains(friendId))
+            {
+                throw new Exception("Friend with this ID was not found");
+            }
+            else
+            {
+                FriendsIds.Remove(friendId);
+            }
+        }
+
+        public void AddMovieListId(int movieListId)
+        {
+            if (MovieListsIds.Contains(movieListId))
+            {
+                throw new Exception("This movie list ID is already in the list");
+            }
+            if (movieListId < 0)
+            {
+                throw new Exception("Movie list's ID cannot be negative");
+            }
+            else
+            {
+                MovieListsIds.Add(movieListId);
+            }
+        }
+
+        public void RemoveMovieListId(int movieListId)
+        {
+            if (!MovieListsIds.Contains(movieListId))
+            {
+                throw new Exception("Movie list with this ID was not found");
+            }
+            else
+            {
+                MovieListsIds.Remove(movieListId);
             }
         }
     }
