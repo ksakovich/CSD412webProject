9b99fe2099f6fa9e959f43200252b536fb096364
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2020-11-12 17:41:23 -0800

    Updated MovieList model

--- CSD412webProject/CSD412webProject/Models/MovieList.cs
+++ CSD412webProject/CSD412webProject/Models/MovieList.cs
@@ -13,13 +13,42 @@ namespace CSD412webProject.Models
         public string MovieListName { get; set; }
         public string MovieListLink { get; set; }
         public bool IsPublic { get; set; }
-
+        public List<int> MovieIds { get; }
         public MovieList(int userId, string name, bool isPublic)
         {
             MovieListId = IdCounter++;
             MovieListName = name;
             IsPublic = isPublic;
             MovieListLink = null;
+            MovieIds = new List<int>();
+        }
+
+        public void AddMovieId(int movieId)
+        {
+            if (MovieIds.Contains(movieId))
+            {
+                throw new Exception("This movie ID is already in the list");
+            }
+            if (movieId < 0)
+            {
+                throw new Exception("movie's ID cannot be negative");
+            }
+            else
+            {
+                MovieIds.Add(movieId);
+            }
+        }
+
+        public void RemoveMovieId(int movieId)
+        {
+            if (!MovieIds.Contains(movieId))
+            {
+                throw new Exception("Movie with this ID was not found");
+            }
+            else
+            {
+                MovieIds.Remove(movieId);
+            }
         }
     }
 }
